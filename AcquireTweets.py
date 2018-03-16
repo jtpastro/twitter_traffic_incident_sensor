3314465acfdde070a8b6171648b81e9756d072b1
Jonata Teixeira Pastro <jtpastro@inf.ufrgs.br>
2018-03-15 23:06:23 -0300

    AcquireTweets.py

--- AcquireTweets.py
+++ AcquireTweets.py
@@ -17,36 +17,38 @@ def loadTweets(tweetFilename, query):
             json.dump(tweets, jsonFile)
         return tweets
 
-def loadFreq(freqFilename, tweets, ngram=1, limit=100):
+def loadFreq(freqFilename, tweets, ngram=2, limit=100):
     jsonPath = Path(freqFilename)
     if jsonPath.exists():
         with jsonPath.open() as jsonFile:
-            return json.load(jsonFile)
+            j = json.load(jsonFile)
+        return j.items()
     else:
         with jsonPath.open('w') as jsonFile:
-            tknzr = RegexpTokenizer(r'[\w-]+')
-            stopwordList = stopwords.words('portuguese')
-            freq = FreqDist(ngrams((word for word in tknzr.tokenize(" ".join(tweets).lower()) if word not in stopwordList and len(word) > 3 and not word[0].isdigit()), ngram)).most_common(limit)
+            tokens = tokenizeTweets(tweets) 
+            freq = dict((' '.join(w),f) for n in range(ngram, 0, -1) for w,f in FreqDist(ngrams(tokens, n)).most_common(limit))
             json.dump(freq, jsonFile)
         return freq
+
+def tokenizeTweets(tweets):
+    tknzr = RegexpTokenizer(r'[\w-]+')
+    stopwordList = stopwords.words('portuguese')
+    return [word for word in tknzr.tokenize(" ".join(tweets).lower()) if word not in stopwordList and len(word) > 3 and not word[0].isdigit()]
+
+def bestGrams(allFreq):
+    freq = {}
+    for w,f in allFreq:
+        if not any(set(w).issubset(o) for o in freq):
+            freq[w] = f
+    return freq
 
 if __name__ == '__main__':
     tFile = 'merged.json'
     fFile = 'freq.json'
     query = 'from:marinapagno OR from:EPTC_POA'
-    query2 = 'from:marinapagno OR from:EPTC_POA'
     tweets = loadTweets(tFile, query)
-    freq = {}
-    for i in range(2,0,-1):
-        for w,f in loadFreq(fFile[:4]+str(i)+fFile[4:], tweets, i):
-            hasBiggerNGram = False
-            w = ' '.join(w)
-            for o in freq:
-                if(set(w).issubset(o)):
-                    hasBiggerNGram = True
-                    break
-            if not hasBiggerNGram:
-                freq[w] = f
-
+    freq = bestGrams(loadFreq(fFile, tweets))
     for w in freq:
         print(w+' => '+str(freq[w]))
+
+    
